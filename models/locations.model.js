50e182e92f2ce20e0424d1dbc728860bdcfae38e
pml68 <tutorialmester@gmail.com>
2024-01-08 19:51:20 +0100

    feat: remove non-existent field

--- models/locations.model.js
+++ models/locations.model.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const LocationsSchema = new Schema({
     address: String,
     phone: String,
-    email: String,
     mondayToThursday: String,
     friday: String,
     saturdayToSunday: String
