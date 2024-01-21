467466cde6b2deedbe9cad784c261e03bff8fd86
pml68 <tutorialmester@gmail.com>
2024-01-21 22:00:17 +0100

    refactor: rename .router.js files to .controller.js

--- controllers/services.controller.js
+++ controllers/services.controller.js
@@ -6,9 +6,8 @@ const getServices = async (req, res, next) => {
         const results = await Services.find({}, {_id: 0});
         if (!results) throw createError(404, 'No available services.');
         res.send(results);
-    } catch (error) {
-        console.log(error.message);
-        next(error);  
+    } catch (err) {
+        console.log(err.message);
     }
 }
 
