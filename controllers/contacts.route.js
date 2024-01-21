eaa72a851b17719a833a0b5607c99d284954702f
pml68 <tutorialmester@gmail.com>
2024-01-21 21:43:06 +0100

    refactor: have better detection for empty collections

--- controllers/contacts.route.js
+++ controllers/contacts.route.js
@@ -4,7 +4,7 @@ const createError = require('http-errors');
 const getContacts = async (req, res, next) => {
     try {
         const results = await Contacts.find({}, {_id: 0, "left._id": 0, "right._id": 0}).sort({_id: 1});
-        if (results == "") throw createError(404, 'No available contact.');
+        if (!results) throw createError(404, 'No available contact.');
         res.send(results)
     } catch (err) {
         console.log(err.message);
--- controllers/locations.route.js
+++ controllers/locations.route.js
@@ -4,7 +4,7 @@ const createError = require('http-errors');
 const getLocations = async (req, res, next) => {
     try {
         const results = await Locations.find({}, {_id: 0});
-        if (results == "") throw createError(404, 'No available locations.');
+        if (!results) throw createError(404, 'No available locations.');
         res.send(results)
     } catch (err) {
         console.log(err.message);
